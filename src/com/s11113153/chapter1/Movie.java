c8036880eb1fcfcb0663c8e5e1ad41bd8433049c
許佑任 <s11113153@stu.edu.tw>
2017-10-15 19:52:34 +0800

    add, Movie: variable/constructor/get/set

--- src/com/s11113153/chapter1/Movie.java
+++ src/com/s11113153/chapter1/Movie.java
@@ -1,4 +1,31 @@
 package com.s11113153.chapter1;
 
 public class Movie {
+  public static final int REGULAR = 0;
+  public static final int NEW_RELEASE = 1;
+  public static final int CHILDRENS = 2;
+
+  private String title;
+  private int price;
+
+  public Movie(String title, int price) {
+    this.title = title;
+    this.price = price;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
 }

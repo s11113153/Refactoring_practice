9ffc78135224cef8dff9f23a26047afd3ffaa4c1
許佑任 <s11113153@stu.edu.tw>
2017-10-15 19:54:23 +0800

    add, Rental: variable/constructor/get

--- src/com/s11113153/chapter1/Rental.java
+++ src/com/s11113153/chapter1/Rental.java
@@ -1,4 +1,19 @@
 package com.s11113153.chapter1;
 
 public class Rental {
+  private Movie movie;
+  private int daysRented;
+
+  public Rental(Movie movie, int daysRented) {
+    this.movie = movie;
+    this.daysRented = daysRented;
+  }
+
+  public int getDaysRented() {
+    return daysRented;
+  }
+
+  public Movie getMovie() {
+    return movie;
+  }
 }
